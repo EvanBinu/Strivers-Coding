720f6dfdd79533463ef3e622143142039d3dce77
Evan Binu <evanbinu17@gmail.com>
2025-12-08 15:39:13 +0530

    ArrayEasy1

---
+++ Arrays/Easy/question1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0;i<n;i++){
+        cin >> arr[i];
+    }
+    int max = arr[0];
+    for(int j = 0;j<n;j++){
+        if(max < arr[j]) max = arr[j];
+    }
+    cout << max << endl;
+}
