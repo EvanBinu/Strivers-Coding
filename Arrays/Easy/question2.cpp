f5cf8bd1c5cb0758e8fb13c92101e4abbc197b18
Evan Binu <evanbinu17@gmail.com>
2025-12-09 23:09:21 +0530

    ArrayEasy2

--- Arrays/Easy/question2.cpp
+++ Arrays/Easy/question2.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 int secondLargest(int arr[],int n){
-    int large = arr[0];
-    for(int i = 0 ; i< n;i++){
-        if(arr[i]>large) large = arr[i];
-    }
+    int large = INT_MIN;
     int second = INT_MIN;
     for(int i = 0 ; i< n;i++){
-        if(arr[i]>second && arr[i] != large) second = arr[i];
+        if(arr[i]>large){
+            second = large;
+            large = arr[i];
+        }
+
+        else if(arr[i]>second && arr[i] != large) second = arr[i];
     }
     return second;
 }
