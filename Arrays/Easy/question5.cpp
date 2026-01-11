19c391b9d6d8865b4623e9fcdf560c294f78bf59
Evan Binu <evanbinu17@gmail.com>
2026-01-11 19:25:08 +0530

    ArrayEasy5

--- Arrays/Easy/question5.cpp
+++ Arrays/Easy/question5.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void leftRotate(int arr[], int n){
+void leftshift(int arr[],int n){
     int first = arr[0];
     for(int i = 1;i<n;i++){
         arr[i-1] = arr[i];
     }
     arr[n-1] = first;
+}
+void rightshift(int arr[],int n){
+    int last = arr[n-1];
+    for(int i = (n-2);i>=0;i--){
+        arr[i+1] = arr[i];
+    }
+    arr[0] = last;
 }
 int main(){
     int n;
@@ -15,9 +22,8 @@ int main(){
     for(int i = 0;i<n;i++){
         cin >> arr[i];
     }
-    leftRotate(arr,n);
-    for(int i = 0;i<n;i++){
-        cout << arr[i] << " ";
+    rightshift(arr,n);
+    for(int x : arr){
+        cout << x << " ";
     }
-    cout << endl;
 }
