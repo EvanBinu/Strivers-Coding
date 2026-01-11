b3f1aa2c639feabd801cbec775e9f4f44dd090dd
Evan Binu <evanbinu17@gmail.com>
2026-01-11 19:16:57 +0530

    ArrayEasy4

--- Arrays/Easy/question4.cpp
+++ Arrays/Easy/question4.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int duplicates(int arr[], int n){
+int remove(int arr[],int n){
     int i = 0;
     for(int j = 1;j<n;j++){
-        if(arr[j]!=arr[i]){
+        if(arr[i]!=arr[j]){
             i++;
             arr[i] = arr[j];
         }
     }
     return i + 1;
 }
+
 int main(){
-    int n;
-    cin >> n;
-    int arr[n];
-    for(int i = 0;i<n;i++){
-        cin >> arr[i];
-    }
-    int val = duplicates(arr,n);
-    for(int i = 0;i<val;i++){
+    int arr[] = {0,0,1,1,1,1,2,2,2,3,3,3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = remove(arr,n);
+    for(int i = 0;i<k;i++){
         cout << arr[i] << " ";
     }
-    cout << endl;
+
 }
