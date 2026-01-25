b74d89cb461c6bd2d4a06b8edd68bb9cee19b06b
Evan Binu <evanbinu17@gmail.com>
2026-01-25 16:44:51 +0530

    ArrayEasy6

--- Arrays/Easy/question6.cpp
+++ Arrays/Easy/question6.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-void rightRotate(int arr[], int n){
-    int last = arr[n-1];
-    for(int i = n-1;i>0;i--){
-        arr[i] = arr[i-1];
-    }
-    arr[0] = last;
+void left_shift_k(vector<int> &arr,int k){
+    reverse(arr.begin(),arr.begin()+k);
+    reverse(arr.begin()+k,arr.end());
+    reverse(arr.begin(),arr.end());    
+}
+void right_shift_k(vector<int> &arr, int k){
+    reverse(arr.begin(),arr.end()); 
+    reverse(arr.begin(),arr.begin()+k);
+    reverse(arr.begin()+k,arr.end());    
 }
 int main(){
-    int n;
-    cin >> n;
-    int arr[n];
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
     for(int i = 0;i<n;i++){
         cin >> arr[i];
     }
-    rightRotate(arr,n);
-    for(int i = 0;i<n;i++){
-        cout << arr[i] << " ";
+    left_shift_k(arr,k);
+    for(int x : arr){
+        cout << x << " ";
     }
     cout << endl;
+    right_shift_k(arr,k);
+    for(int x : arr){
+        cout << x << " ";
+    }
+    cout << endl;
+
 }
