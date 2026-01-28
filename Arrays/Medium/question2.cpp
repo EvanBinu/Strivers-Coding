2c922fe8916d40c95cdcea3a121b86e54cd754d9
Evan Binu <evanbinu17@gmail.com>
2026-01-28 19:06:01 +0530

    ArrayMedium2

---
+++ Arrays/Medium/question2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0;i<n;i++){
+        cin >> arr[i];
+    }
+    int low=0,mid=0,high=n-1;
+    for(int i = 0;i<n;i++){
+        if(arr[mid] == 1){
+            mid++;
+        }
+        else if(arr[mid] == 0){
+            swap(arr[mid],arr[low]);
+            mid++;
+            low++;
+        }
+        else if(arr[mid] == 2){
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
+    for(int x : arr){
+        cout << x << " ";
+    }
+    cout << endl;
+}
