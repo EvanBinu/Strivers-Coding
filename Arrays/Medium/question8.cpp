58014702b657e2e7f5c8c7a651f37067ead2251a
Evan Binu <evanbinu17@gmail.com>
2026-02-02 21:38:14 +0530

    ArrayMedium8

--- Arrays/Medium/question8.cpp
+++ Arrays/Medium/question8.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
     int n;
     cin >> n;
-    int arr[n];
+    vector<int> arr;
     for(int i = 0;i<n;i++){
-        cin >> arr[i];
+        int ele;
+        cin >> ele;
+        arr.push_back(ele);
     }
-    for(int i = 0;i<n;i++){
-        if(arr[i]==n){
-            arr[i] = 1;
+    int brk = -1;
+    for(int i = n-2;i>-1;i--){
+        if(arr[i] < arr[i+1]){
+            brk = i;
+            break;
         }
-        else{
-            arr[i]++;
+    }
+    if(brk <0){
+        reverse(arr.begin(),arr.end());
+    }
+    else{
+        int brk1=-1;
+        for(int i = n-1;i>0;i--){
+            if(arr[i] > arr[brk]){
+                brk1 = i;
+                break;
+            }
         }
+        swap(arr[brk],arr[brk1]);
+        reverse(arr.begin()+brk+1,arr.end());
     }
     for(int x : arr){
         cout << x << " ";
