3e38810934dd4d70e0c85cfa41d5d143e3bca459
Evan Binu <evanbinu17@gmail.com>
2026-02-03 18:36:45 +0530

    ArrayMedium12

---
+++ Arrays/Medium/question12.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n][n];
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            cin >> arr[i][j];
+        }
+    }
+    for(int i = 0;i<n;i++){
+        for(int j = i+1;j<n;j++){
+            swap(arr[i][j],arr[j][i]);
+        }
+    }
+    for(int i = 0;i<n;i++){
+        reverse(arr[i],arr[i]+n);
+    }
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            cout << arr[i][j]<< " ";
+        }
+        cout << endl;
+    }
+    
+
+}
