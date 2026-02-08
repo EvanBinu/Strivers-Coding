e5f99af07f548ad5298d8d776dbb1e9136002f2c
Evan Binu <evanbinu17@gmail.com>
2026-02-08 23:04:52 +0530

    ArrayHard11

---
+++ Arrays/Hard/question11.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int countpairs(int arr[],int l,int r,int mid){
+    int count= 0;
+    int j = mid+1;
+    for(int i = l;i<=mid;i++){
+        while(j<=r && arr[i] > 2* arr[j]){
+            j++;
+        }
+        count+=(j-(mid+1));
+    }
+    return count;
+
+}
+int merge(int arr[],int l,int r,int mid){
+    int n1 = mid-l+1;
+    int n2 = r-mid;
+    int L[n1];
+    int R[n2];
+    for(int i = 0;i<n1;i++){
+        L[i] = arr[l+i];
+    }
+    for(int i = 0;i<n2;i++){
+        R[i] = arr[mid+i+1];
+    }
+    int i = 0 , j = 0, k= l;
+    int swap = 0;
+    while(i<n1 && j<n2){
+        if(L[i]<=R[j]){
+            arr[k++] = L[i++];
+
+        }
+        else{
+            arr[k++] = R[j++];
+            swap+=(n1-i);
+
+        }
+    }
+    while(i < n1) arr[k++] = L[i++];
+    while(j < n2) arr[k++] = R[j++];
+    return swap;
+}
+int mergesort(int arr[],int l,int r){
+    int count = 0;
+    if (l<r){
+        int mid = (l+r)/2;
+        count+=mergesort(arr,l,mid);
+        count+=mergesort(arr,mid+1,r);
+        count+=countpairs(arr,l,r,mid);
+        merge(arr,l,r,mid);
+    }
+    return count;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0;i<n;i++){
+        cin >> arr[i];
+    }
+    cout << mergesort(arr,0,n-1) << endl;
+
+}
