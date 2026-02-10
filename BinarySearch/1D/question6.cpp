985c7d4178b87c843788dda552db06baa62ff95c
Evan Binu <evanbinu17@gmail.com>
2026-02-10 23:01:53 +0530

    BinaryEasy6

---
+++ BinarySearch/1D/question6.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n,x;
+    cin >> n >> x;
+    vector<int> nums(n);
+    for(int i = 0;i<n;i++){
+        cin >> nums[i];
+    }
+    int low = 0;
+    int high = n -1;
+    int ans = -1;
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        if(nums[mid] == x){
+            ans = mid;
+            low = mid + 1;
+        }
+        else if(nums[mid] > x){
+            high = mid - 1;
+        }
+        else if(nums[mid] < x){           
+            low = mid + 1;
+        }
+    }
+    cout << ans << endl;
+}
