bb4cf61058417af22d95cda9593c741cbf62b06a
Evan Binu <evanbinu17@gmail.com>
2026-02-12 09:02:38 +0530

    BinaryEasy10&11

---
+++ BinarySearch/1D/question10.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int rotatedminsearch(vector<int>& nums){
+    int n = nums.size();
+    int low = 0;
+    int high = n - 1;
+    int ans = -1;
+    while(low<high){
+        int mid = low + (high-low)/2;
+        if(nums[mid] > nums[high]){
+            low = mid + 1;
+        }
+        else{
+            high = mid;
+        }
+    }
+    
+    return nums[low];
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0;i<n;i++){
+        cin >> nums[i]; 
+    }
+
+    cout << rotatedminsearch(nums) << endl;
+    
+}
