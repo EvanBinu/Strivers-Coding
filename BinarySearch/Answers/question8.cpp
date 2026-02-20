1b5cc8efc55757a2340bb9258b953e4a46f10537
Evan Binu <evanbinu17@gmail.com>
2026-02-20 22:24:39 +0530

    BinaryA8

--- BinarySearch/Answers/question8.cpp
+++ BinarySearch/Answers/question8.cpp
@@ -3,6 +3,38 @@
 #include <algorithm>
 using namespace std;
 
+bool canplace(vector<int>& nums, int k, int d){
+    int count = 1;
+    int pos = nums[0];
+    for(int i = 1;i<nums.size();i++){
+        if(nums[i] - pos >=d){
+            count++;
+            pos = nums[i];
+        }
+        if(count >= k) return true;
+    }
+    return false;
+
+}
+int search(vector<int>& nums,int k){
+    sort(nums.begin(),nums.end());
+    int ans = 0;
+    int low = 1;
+    int high = nums.back() - nums.front();
+    while(low<=high){
+        int mid = low + (high - low)/2;
+        if(canplace(nums,k,mid)){
+            ans = mid;
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    return ans;
+
+}
+
 int main(){
     int n,k;
     cin >> n >>k;
@@ -10,13 +42,7 @@ int main(){
     for(int i = 0;i<n;i++){
         cin >> nums[i];
     }
-    sort(nums.begin(),nums.end());
-    vector<int> distance;
-    distance.push_back(0);
-    for(int i = 1;i<n;i++){
-        distance.push_back(nums[i] - nums[i-1]);
-    }
-    for(int x : distance){
-        cout << x << " ";
-    }
+    int result = search(nums,k);
+    cout << result<<endl;
+    
 }
