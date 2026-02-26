3d888b5437c82c2bbcf765d911120c9ef801a1cb
Evan Binu <evanbinu17@gmail.com>
2026-02-26 21:58:23 +0530

    StringEasy3

---
+++ Strings/Easy/question3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        string result = "";
+        for(int i = num.size()-1 ;i>-1;i--){
+            int d = (num[i] - '0');
+            if(d%2==1){
+                result+=num.substr(0,i+1);
+                break;
+            }
+        }
+        return result;
+    }
+};
+
