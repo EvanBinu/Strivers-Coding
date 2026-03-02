66c4940a1dab4c225c4a660714d3fc7ac3c8796b
Evan Binu <evanbinu17@gmail.com>
2026-03-02 23:54:27 +0530

    StringsMedium1

---
+++ Strings/Medium/question1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    static bool comparator(const pair<int, char>& p1, const pair<int, char>& p2){
+        if(p1.first != p2.first) return p1.first > p2.first;
+        return p1.second < p2.second;
+    }
+    string frequencySort(string s) {
+        pair<int,char> freq[128];
+        for(int i = 0;i<128;i++){
+            freq[i] = {0, (char)i};
+        }
+        for(char ch : s) {
+            freq[(unsigned char)ch].first++;
+        }
+        sort(freq,freq+128,comparator);
+        string ans = "";
+        for(int i = 0;i<128;i++){
+            if(freq[i].first>0) ans.append(freq[i].first, freq[i].second);
+        }
+        return ans;
+    }
+};
