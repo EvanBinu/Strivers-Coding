a40e05b7da597cef3113502273705ee5507a7859
Evan Binu <evanbinu17@gmail.com>
2026-03-11 18:14:34 +0530

    DLLintro4

---
+++ LinkedList/DoubleLinkedList/ReverseDLL.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int data1){
+        this->data=data1;
+        this->next=nullptr;
+        this->prev=nullptr;
+    }
+    Node(int data1,Node* next, Node* prev){
+        this->data = data1;
+        this->next = next;
+        this->prev = prev;
+    }
+    static void insertEnd(Node* head,int val){
+        if (head == nullptr) return;
+        Node* temp = head;
+        while(temp->next!=nullptr){
+            temp = temp->next;
+        }
+        Node* node = new Node(val,nullptr,temp);
+        temp->next = node;
+    }
+    static void deleteEnd(Node* head){
+        if(head == nullptr) return;
+        if(head->next == nullptr){
+            delete head;
+        }
+        Node* temp = head;
+        while(temp->next !=nullptr){
+            temp = temp->next;
+        }
+        temp->prev->next = nullptr;
+        delete temp;
+    }
+    static Node* reverse(Node* head){
+        Node* curr = head;
+        Node* lastV = nullptr;
+        while(curr!=nullptr){
+            lastV = curr->prev;
+            curr->prev = curr->next;
+            curr->next = lastV;
+            curr = curr->prev;
+        }
+        return lastV->prev;
+    }
+    static void printlist(Node* head){
+        Node* temp = head;
+        while(temp!=nullptr){
+            cout << temp->data<<" ";
+            temp = temp->next;
+        }
+        cout << endl;
+
+    }
+};
+
+int main(){
+    Node* head = new Node(100);
+    head->next = new Node(200);
+    head->next->prev = head;
+    head->next->next = new Node(300);
+    head->next->next->prev = head->next;
+
+    Node::printlist(head);
+    Node* head1 = Node::reverse(head);
+    Node::printlist(head1);
+}
